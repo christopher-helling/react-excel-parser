388d4b01157bef11abd5b60c6126048e87c401af
Chris Helling <chris.helling@tcw.com>
2023-03-14 21:04:07 -0700

    init commit

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export function ExcelRenderer(file: any, callback: any): any;
+export class OutTable {
+    constructor(props: any);
+    state: {};
+    render(): any;
+}
+//# sourceMappingURL=index.d.ts.map
